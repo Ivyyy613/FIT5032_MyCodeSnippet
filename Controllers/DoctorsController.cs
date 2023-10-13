c6fe9644becea81f473bedc1d2b0ac343e895169
Ivyyy613 <128117442+Ivyyy613@users.noreply.github.com>
2023-10-13 15:13:36 +1100

    adding all controler and models

---
+++ Controllers/DoctorsController.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Core;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032_EasyX.Models;
+using FIT5032_EasyX.Util;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace FIT5032_EasyX.Controllers
+{
+    [Authorize]
+    public class DoctorsController : Controller
+    {
+        private Entities db = new Entities();
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET: Doctors
+        public ActionResult Index()
+        {
+            var doctorRatings = db.BookingsSet
+                .Where(b => b.Rating > 0)
+                .GroupBy(b => b.DoctorId)
+                .Select(g => new
+                {
+                    DoctorId = g.Key,
+                    AverageRating = g.Average(b => b.Rating)
+                })
+                .ToDictionary(d => d.DoctorId, d => d.AverageRating);
+
+            ViewBag.DoctorRatings = doctorRatings;
+            return View(db.DoctorSet.ToList());
+        }
+
+        // GET: Doctors/Details/5
+        [Authorize(Roles ="admin")]
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = db.DoctorSet.Find(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+        [Authorize(Roles = "admin,doctor")]
+        public ActionResult MyDetails()
+        {
+            string id = User.Identity.GetUserId();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = db.DoctorSet.Find(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        //// GET: Doctors/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Doctors/Create
+        //// To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName,Title,First_Name,Last_Name,Major,Address,Is_Ready_For_Visitor")] Doctor doctor)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.AspNetUsers.Add(doctor);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(doctor);
+        //}
+
+        // GET: Doctors/Create/5
+        [Authorize]
+
+        public ActionResult Create(string id)
+        {
+            if (id == null || id != User.Identity.GetUserId())
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = new Doctor()
+            {
+                Id = id
+            };
+            return View(doctor);
+        }
+
+        // POST: Doctors/Create/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+
+        public ActionResult Create([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName,Title,First_Name,Last_Name,Major,Address,Is_Ready_For_Visitor")] Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                AspNetUsers aspNetUsers = db.AspNetUsers.Find(doctor.Id);
+                db.AspNetUsers.Remove(aspNetUsers);
+
+                Doctor doctorObj = new Doctor(aspNetUsers, doctor);
+                db.DoctorSet.Add(doctorObj);
+                db.SaveChanges();
+                // Assign doctor role to this new doctor
+                UserManager.AddToRole(doctorObj.Id, StaticRole.doctor);
+                return RedirectToAction("Index");
+            }
+            return View(doctor);
+        }
+
+        // GET: Doctors/Edit/5
+        [Authorize(Roles = "admin,doctor")]
+
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = db.DoctorSet.Find(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        // POST: Doctors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin,doctor")]
+
+        public ActionResult Edit([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName,Title,First_Name,Last_Name,Major,Address,Is_Ready_For_Visitor")] Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(doctor).State = EntityState.Modified;
+                db.SaveChanges();
+                if (User.IsInRole("doctor"))                
+                    return RedirectToAction("MyDetails");
+                if (User.IsInRole("admin"))
+                    return RedirectToAction("Index");
+            }
+            return View(doctor);
+        }
+
+        // GET: Doctors/Delete/5
+        [Authorize(Roles = "admin,doctor")]
+
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = db.DoctorSet.Find(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        // POST: Doctors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin,doctor")]
+
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Doctor doctor = db.DoctorSet.Find(id);
+            db.AspNetUsers.Remove(doctor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/PatientsController.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032_EasyX.Models;
+using FIT5032_EasyX.Util;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+
+namespace FIT5032_EasyX.Controllers
+{
+    [Authorize]
+    public class PatientsController : Controller
+    {
+        private Entities db = new Entities();
+        private ApplicationUserManager _userManager;
+        private ApplicationSignInManager _signInManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        // GET: Patients
+        [Authorize(Roles = "admin")]
+        public ActionResult Index()
+        {
+            return View(db.PatientSet.ToList());
+        }
+        [Authorize(Roles = "admin")]
+        // GET: Patients/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.PatientSet.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+        [Authorize(Roles = "admin,patient")]
+        public ActionResult MyDetails()
+        {
+            String id = User.Identity.GetUserId();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.PatientSet.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+        [Authorize]
+        // GET: Patients/Create
+        public ActionResult Create(string id)
+        {
+            if (id == null || id != User.Identity.GetUserId())
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = new Patient()
+            {
+                Id = id
+            };
+            return View(patient);
+        }
+
+        // POST: Patients/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Create([Bind(Include = "Id,Title,First_Name,Last_Name,Date_Of_Birth,Address")] Patient patient)
+
+        //public async Task<ActionResult> Create([Bind(Include = "Id,Title,First_Name,Last_Name,Date_Of_Birth,Address")] Patient patient)
+        {
+
+            if (ModelState.IsValid)
+            {
+                //var externalLogin = UserManager.GetLogins(aspNetUsers.Id);
+                var temp = db.AspNetUserLogins.Where(login => login.UserId == patient.Id).FirstOrDefault();
+
+                AspNetUsers aspNetUsers = db.AspNetUsers.Find(patient.Id);
+                db.AspNetUsers.Remove(aspNetUsers);
+                Patient patientObj = new Patient(aspNetUsers, patient);
+
+                db.PatientSet.Add(patientObj);
+                //db.AspNetUserLogins.Add(temp);
+                db.SaveChanges();
+                if (temp != null)
+                    db.AspNetUserLogins.Add(temp);
+                db.SaveChanges();
+                UserManager.AddToRole(patientObj.Id, StaticRole.patient);
+                //await SignInManager.SignInAsync(aspNetUsers, isPersistent: false, rememberBrowser: false);
+                //return RedirectToAction("Index");
+                AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View(patient);
+        }
+        [Authorize(Roles = "admin,patient")]
+        // GET: Patients/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.PatientSet.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patients/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin,patient")]
+        public ActionResult Edit([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName,Title,First_Name,Last_Name,Date_Of_Birth,Address")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(patient).State = EntityState.Modified;
+                db.SaveChanges();
+                if (User.IsInRole("patient"))
+                    return RedirectToAction("MyDetails");
+                if (User.IsInRole("admin"))
+                    return RedirectToAction("Index");
+            }
+            return View(patient);
+        }
+
+        // GET: Patients/Delete/5
+        [Authorize(Roles = "admin")]
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.PatientSet.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin")]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Patient patient = db.PatientSet.Find(id);
+            db.PatientSet.Remove(patient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
